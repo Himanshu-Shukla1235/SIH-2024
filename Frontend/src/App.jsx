2a9dd7d0b6aeaa24a80cbac4d8142203708cf23e
Blurix23 <himansh2096@..12>
2024-08-24 09:30:14 +0530

    testing setup complete

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Testing from "./pages/Test";
+
+function App() {
+  return (
+    <>
+      <header>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Testing />} />
+          </Routes>
+        </BrowserRouter>
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
