ecffed97b3b35609f082c2c2be6b5a800c7529fa
Blurix23 <145251086+Blurix23@users.noreply.github.com>
2024-08-25 23:15:04 +0530

    homepage...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Testing from "./pages/Test";
+import Home from "./pages/Home"
 // import Card from './components/Card'
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
       <header>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Testing />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/test" element={<Testing />} />
           </Routes>
         </BrowserRouter>
       </header>
