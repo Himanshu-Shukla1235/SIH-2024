1d91ca5d070befb4f6dc26d34571840276f6358a
amgubrele <amgubrele011@gmail.com>
2024-08-26 15:07:35 +0530

    a

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,30 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../../components/navBar/navBar';
-import './Home.css'
-import ContactButton from "../../components/navBar/contactButton"
+import './Home.css';
 
 const Home = () => {
+  const [showNavBar, setShowNavBar] = useState(true);
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollTop = window.scrollY;
+
+      if (currentScrollTop > lastScrollTop) {
+        setShowNavBar(false);
+      } else {
+        setShowNavBar(true);
+      }
+
+      setLastScrollTop(currentScrollTop <= 0 ? 0 : currentScrollTop);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [lastScrollTop]);
+
   return (
     <React.Fragment>
-        <NavBar/>
-        <div className='box_1'>
-            <div className="sec_1">
-                //paste comp1 here 
-                <a href="#">ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</a>
-            </div>
-            <div className="sec_2">
-                //paste comp1 here 
-            </div>
-            //rest comp
-        </div>
-        <div className="background">
+
+      {/* nav bar logic */}
+      <div className={`navComponent ${showNavBar ? 'navComponentShow' : 'navComponentHide'}`}>
+        <NavBar />
+      </div>
+
+      {/* section1 */}
+      <div className="sec_1">
+        {/* Paste comp1 here */}
+        <a href="#">ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</a>
+      </div>
+
+      {/* //background */}
+      <div className="background">
         <div className="filter"></div>
-        </div>
-        
-        
-      
+      </div>
+
+      {/* rest sectins */}
+      <div className='restSections'>
+          <div className="section2">
+
+          </div>
+          <div className="section2">
+            
+          </div>
+      </div>
     </React.Fragment>
   );
-}
+};
 
 export default Home;
