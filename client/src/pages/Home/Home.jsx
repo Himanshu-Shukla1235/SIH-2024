2cad5987befad25fe6be2d404a5701c1c9d12d3b
Blurix23 <145251086+Blurix23@users.noreply.github.com>
2024-08-26 14:26:15 +0530

    home sec1

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import NavBar from '../../components/navBar/navBar';
-import './Home.css'
-import ContactButton from "../../components/navBar/contactButton"
+import React from "react";
+import NavBar from "../../components/navBar/navBar";
+import "./Home.css";
+import ContactButton from "../../components/navBar/contactButton";
 
 const Home = () => {
   return (
     <React.Fragment>
-        <NavBar/>
-        <div className='box_1'>
-            <div className="sec_1">
-                //paste comp1 here 
-                <a href="#">ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</a>
-            </div>
-            <div className="sec_2">
-                //paste comp1 here 
-            </div>
-            //rest comp
+      <NavBar />
+      <div className="box_1">
+        <div className="sec_1">
+          <div className="sec11">
+            <div className="sec111">text</div>
+            <div className="sec112">buttons</div>
+          </div>
+          <div className="sec12">charts</div>
         </div>
-        <div className="background">
+        {/* ___________________________________________section-2_____________________________________________________ */}
+        <div className="sec_2"></div>
+        <div className="sec_3"></div>
+        <div className="sec_4"></div>
+      </div>
+      <div className="background">
         <div className="filter"></div>
-        </div>
-        
-        
-      
+      </div>
     </React.Fragment>
   );
-}
+};
 
 export default Home;
