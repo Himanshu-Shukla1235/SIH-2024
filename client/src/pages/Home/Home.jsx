b8bf3c329ede258917dd6ac379d51209d89e4988
PratyushDube-ygit config --global user.name PratyushDube-ygit config --global user.name PratyushDube-y <pratyushdubey189@gmail.com>
2024-08-26 15:20:21 +0530

    pull

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,30 +1,41 @@
-import React from 'react';
-import NavBar from '../../components/navBar/navBar';
-import './Home.css'
-import ContactButton from "../../components/navBar/contactButton"
+import React from "react";
+import NavBar from "../../components/navBar/navBar";
+import "./Home.css";
+// import ContactButton from "../../components/navBar/contactButton"
 
 const Home = () => {
   return (
     <React.Fragment>
-        <NavBar/>
-        <div className='box_1'>
-            <div className="sec_1">
-                //paste comp1 here 
-                <a href="#">ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</a>
-            </div>
-            <div className="sec_2">
-                //paste comp1 here 
-            </div>
-            //rest comp
+      <NavBar />
+      <div className="box_1">
+        <div className="sec_1">
+          <a href="#">
+            ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+          </a>
         </div>
-        <div className="background">
+
+        <div className="sec_2">
+          <div className="sec_2-2">
+            <div className="sec_2-3">
+              <h2>Tailored Solutions for Every Mine</h2>
+              <p>
+                Whether you{`'`}re overseeing a small operation or managing
+                multiple sites, our scalable platform adapts to your unique
+                needs.
+              </p>
+              <button>Explore plans</button>
+            </div>
+            <img src="/public/images/plant1.jpg" alt="Plant1" />
+          </div>
+        </div>
+
+        {/* rest comp */}
+      </div>
+      <div className="background">
         <div className="filter"></div>
-        </div>
-        
-        
-      
+      </div>
     </React.Fragment>
   );
-}
+};
 
 export default Home;
