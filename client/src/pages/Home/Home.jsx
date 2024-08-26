221c041719b8209b7888b854e8eb7c405098f1a8
Blurix23 <145251086+Blurix23@users.noreply.github.com>
2024-08-26 15:33:56 +0530

    pull

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
       {/* section1 */}
       <div className="sec_1">
         {/* Paste comp1 here */}
-        <a href="#">ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</a>
+     
       </div>
 
       {/* //background */}
