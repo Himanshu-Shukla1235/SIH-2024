78a28f0e66e83566e7ecec31068d243e2fd38f1f
Blurix23 <himansh2096@..12>
2024-08-27 12:24:25 +0530

    comit

--- client/src/components/HomePage/section1/slider.jsx
+++ client/src/components/HomePage/section1/slider.jsx
@@ -1,31 +1,36 @@
 import React, { useState } from "react";
 import "./slider.css";
-import Button from "./Button1"; // Capitalize 'Button' as it's a React component
 
+import ButtonComponent from "./Button2";
+import Button from "./Button1";
 
 const Slider = ({ slides }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const nextSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === slides.length - 1 ? 0 : prevIndex + 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === slides.length - 1 ? 0 : prevIndex + 1
+    );
   };
 
   const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? slides.length - 1 : prevIndex - 1));
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? slides.length - 1 : prevIndex - 1
+    );
   };
 
   return (
     <div className="slideContent">
       <div className="show">
         {slides.length > 0 && (
-          <div className="show1">
-            {slides[currentIndex].content}
-          </div>
+          <div className="show1">{slides[currentIndex].content}</div>
         )}
       </div>
       <div className="slider-nav">
-      <button onClick={prevSlide}>prev</button>
-      <button onClick={nextSlide}>next</button>
+        
+        <ButtonComponent onClick={prevSlide} iconDirection="left" ></ButtonComponent>
+        <ButtonComponent onClick={nextSlide} iconDirection="right" ></ButtonComponent>
+
         {/* <div>{currentIndex}</div> */}
       </div>
     </div>
