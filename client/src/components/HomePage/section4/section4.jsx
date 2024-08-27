4c2da8dc46c9a4130fc5137d86f58e47fadc9783
PratyushDube-ygit config --global user.name PratyushDube-ygit config --global user.name PratyushDube-y <pratyushdubey189@gmail.com>
2024-08-27 11:03:54 +0530

    lelo

---
+++ client/src/components/HomePage/section4/section4.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import './section4.css'
+
+function section4() {
+  return (
+  <div className="sec_4">
+    <img src="/public/images/plant2.png" alt="plant-2" />
+  </div>
+  )
+}
+
+export default section4;
