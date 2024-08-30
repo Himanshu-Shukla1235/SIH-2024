bc961609bd65b508a83485c3bf5eb869f6d7053f
Blurix23 <himansh2096@..12>
2024-08-31 02:34:48 +0530

    pulling

--- client/src/components/CompanyDashBoard/section2/section2.jsx
+++ client/src/components/CompanyDashBoard/section2/section2.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./section2.css";
-import Graph1  from './graph1'
-import Graph2  from './graph2'
+import Graph1 from "./graph1";
+import Graph2 from "./graph2";
 const section2 = () => {
   return (
-    <div className="section_2">
-      <div className="section_21"><h4>Sinkgap Graph</h4><Graph1></Graph1></div>
-      <div className="section_22"><h4>Emmision by different types of sources </h4><Graph2></Graph2></div>
+    <div className="section_2"><div className="overallInfo">hello</div>
+      <div className="charts">
+        <div className="section_21">
+          <h4>Sinkgap Graph</h4>
+          <Graph1></Graph1>
+        </div>
+        <div className="section_22">
+          <h4>Emmision by different types of sources </h4>
+          <Graph2></Graph2>
+        </div>
+      </div>
+      
     </div>
   );
 };
