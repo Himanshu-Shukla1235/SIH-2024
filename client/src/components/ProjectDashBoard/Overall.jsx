98a91b13cc4bf34c930d7f2b480dea9f3b6650d0
Blurix23 <himansh2096@..12>
2024-09-05 01:02:36 +0530

    plling

--- client/src/components/ProjectDashBoard/Overall.jsx
+++ client/src/components/ProjectDashBoard/Overall.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import "./Overall.css";
 const Overall = () => {
   return (
     <React.Fragment>
-        <main className="proD_overall_main">
-            <div className="proD_profile">
-                profile
-            </div>
-        </main>
+      <main className="proD_overall_main">
+        <div className="proD_overall_box1"><h2 >Project Name</h2></div>
+        <div className="proD_overall_box2">2</div>
+        <div className="proD_overall_box3">3</div>
+      </main>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Overall
+export default Overall;
