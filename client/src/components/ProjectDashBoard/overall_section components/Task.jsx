8e92c509d4c8ab488531e23233b0dd77a22edaad
Blurix23 <himansh2096@..12>
2024-09-05 03:08:54 +0530

    puahing

--- client/src/components/ProjectDashBoard/overall_section components/Task.jsx
+++ client/src/components/ProjectDashBoard/overall_section components/Task.jsx
@@ -9,6 +9,19 @@ const Task = () => {
     { id: 3, title: 'Monitor emission levels', completed: false },
     { id: 4, title: 'Schedule safety drills', completed: true },
     { id: 5, title: 'Report daily production', completed: false },
+    { id: 6, title: 'Report daily production', completed: false },
+    { id: 7, title: 'Report daily production', completed: false },
+    { id: 8, title: 'Report daily production', completed: false },
+    { id: 9, title: 'Report daily production', completed: false },
+    { id: 10, title: 'Report daily production', completed: false },
+    { id: 11, title: 'Report daily production', completed: false },
+    { id: 12, title: 'Report daily production', completed: false },
+    { id: 13, title: 'Report daily production', completed: false },
+    { id: 14, title: 'Report daily production', completed: false },
+    { id: 15, title: 'Report daily production', completed: false },
+    { id: 16, title: 'Report daily production', completed: false },
+    { id: 17, title: 'Report daily production', completed: false },
+    { id: 18, title: 'Report daily production', completed: false },
   ]);
 
   return (
